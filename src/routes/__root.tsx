516e5eff50251dd46bd93803221f80bca5a876b2
1F10250041 <s1F102500416@iniad.org>
2026-09-23 21:59:23 +0900

    format check

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -23,7 +23,11 @@ export const Route = createRootRoute({
         href: "/favicon-96x96.png",
       },
       { rel: "shortcut icon", href: "/favicon.ico" },
-      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
       { rel: "manifest", href: "/site.webmanifest" },
     ],
   }),
